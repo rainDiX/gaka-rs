c6ba06161005627348d727cffde1448e2f6ce10d
Romain Dijoux <raindx@irised.one>
2024-07-22 01:18:44 +0200

    lint build.rs

--- build.rs
+++ build.rs
@@ -1,10 +1,9 @@
-use shaderc;
 use std::{fs, path};
 
 fn build_hlsl_shader(file_path: &str, options: &shaderc::CompileOptions) {
     let compiler = shaderc::Compiler::new().unwrap();
     let shader_source =
-        fs::read_to_string(file_path).expect(&format!("failed to read {}", file_path));
+        fs::read_to_string(file_path).unwrap_or_else(|_| panic!("failed to read {}", file_path));
 
     let vertex_shader_entry = "VSMain";
     let fragment_shader_entry = "PSMain";
@@ -22,52 +21,52 @@ fn build_hlsl_shader(file_path: &str, options: &shaderc::CompileOptions) {
             &shader_source,
             shaderc::ShaderKind::Vertex,
             file_path,
-            &vertex_shader_entry,
-            Some(&options),
+            vertex_shader_entry,
+            Some(options),
         )
         .unwrap();
 
     let out_path = format!("assets/spirv/{}.vert.spv", out);
-    fs::write(&out_path, binary_result.as_binary_u8()).unwrap();
+    fs::write(out_path, binary_result.as_binary_u8()).unwrap();
 
     let text_result = compiler
         .compile_into_spirv_assembly(
             &shader_source,
             shaderc::ShaderKind::Vertex,
             file_path,
-            &vertex_shader_entry,
-            Some(&options),
+            vertex_shader_entry,
+            Some(options),
         )
         .unwrap();
 
     let out_path = format!("assets/spirv/{}.vert.spvasm", out);
-    std::fs::write(&out_path, &text_result.as_text()).unwrap();
+    std::fs::write(out_path, text_result.as_text()).unwrap();
 
     let binary_result = compiler
         .compile_into_spirv(
             &shader_source,
             shaderc::ShaderKind::Fragment,
             file_path,
-            &fragment_shader_entry,
-            Some(&options),
+            fragment_shader_entry,
+            Some(options),
         )
         .unwrap();
 
     let out_path = format!("assets/spirv/{}.frag.spv", out);
-    fs::write(&out_path, binary_result.as_binary_u8()).unwrap();
+    fs::write(out_path, binary_result.as_binary_u8()).unwrap();
 
     let text_result = compiler
         .compile_into_spirv_assembly(
             &shader_source,
             shaderc::ShaderKind::Fragment,
             file_path,
-            &fragment_shader_entry,
-            Some(&options),
+            fragment_shader_entry,
+            Some(options),
         )
         .unwrap();
 
     let out_path = format!("assets/spirv/{}.frag.spvasm", out);
-    std::fs::write(&out_path, &text_result.as_text()).unwrap();
+    std::fs::write(out_path, text_result.as_text()).unwrap();
 }
 
 // Example custom build script.
@@ -76,15 +75,14 @@ fn main() {
     options.set_source_language(shaderc::SourceLanguage::HLSL);
 
     println!("cargo::rerun-if-changed=shaders/basic.hlsl");
-    let hlsl_shaders = vec!["shaders/basic.hlsl"];
+    let hlsl_shaders = ["shaders/basic.hlsl"];
 
     if path::Path::new("assets/spirv").exists() {
         fs::remove_dir_all("assets/spirv").expect("Failed to recreate oupput dir");
     }
     fs::create_dir_all("assets/spirv").expect("Failed to create output dir");
 
-
     hlsl_shaders
         .iter()
-        .for_each(|s| build_hlsl_shader(&s, &options));
+        .for_each(|s| build_hlsl_shader(s, &options));
 }
